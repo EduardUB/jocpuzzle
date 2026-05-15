737140fe05c045eefabea57d0c8faa1ebbd93716
EduardUB <eduardmatainoguera@ub.edu>
2026-05-15 13:49:02 +0200

    levels.js

--- levels.js
+++ levels.js
@@ -1,109 +1,26 @@
 const LEVELS = [
-
   {
-    name: "Classic",
-
+    name: "Classic Real",
     pieces: [
+      // bloc gran (objectiu)
+      { id: 1, x: 1, y: 0, w: 2, h: 2, color: "red", direction: "both" },
 
-      {
-        id: 1,
-        x: 1,
-        y: 0,
-        w: 2,
-        h: 2,
-        color: "red",
-        direction: "both"
-      },
+      // laterals verticals
+      { id: 2, x: 0, y: 0, w: 1, h: 2, color: "blue", direction: "vertical" },
+      { id: 3, x: 3, y: 0, w: 1, h: 2, color: "blue", direction: "vertical" },
 
-      {
-        id: 2,
-        x: 0,
-        y: 0,
-        w: 1,
-        h: 2,
-        color: "blue",
-        direction: "vertical"
-      },
+      { id: 4, x: 0, y: 2, w: 1, h: 2, color: "blue", direction: "vertical" },
+      { id: 5, x: 3, y: 2, w: 1, h: 2, color: "blue", direction: "vertical" },
 
-      {
-        id: 3,
-        x: 3,
-        y: 0,
-        w: 1,
-        h: 2,
-        color: "blue",
-        direction: "vertical"
-      },
+      // peça central
+      { id: 6, x: 1, y: 2, w: 2, h: 1, color: "blue", direction: "horizontal" },
 
-      {
-        id: 4,
-        x: 0,
-        y: 2,
-        w: 1,
-        h: 2,
-        color: "blue",
-        direction: "vertical"
-      },
+      // blocs petits (exactes)
+      { id: 7, x: 1, y: 3, w: 1, h: 1, color: "yellow", direction: "both" },
+      { id: 8, x: 2, y: 3, w: 1, h: 1, color: "yellow", direction: "both" },
 
-      {
-        id: 5,
-        x: 3,
-        y: 2,
-        w: 1,
-        h: 2,
-        color: "blue",
-        direction: "vertical"
-      },
-
-      {
-        id: 6,
-        x: 1,
-        y: 2,
-        w: 2,
-        h: 1,
-        color: "blue",
-        direction: "horizontal"
-      },
-
-      {
-        id: 7,
-        x: 1,
-        y: 3,
-        w: 1,
-        h: 1,
-        color: "yellow",
-        direction: "both"
-      },
-
-      {
-        id: 8,
-        x: 0,
-        y: 4,
-        w: 1,
-        h: 1,
-        color: "yellow",
-        direction: "both"
-      },
-
-      {
-        id: 9,
-        x: 2,
-        y: 4,
-        w: 1,
-        h: 1,
-        color: "yellow",
-        direction: "both"
-      },
-
-      {
-        id: 10,
-        x: 3,
-        y: 4,
-        w: 1,
-        h: 1,
-        color: "yellow",
-        direction: "both"
-      }
+      { id: 9, x: 1, y: 4, w: 1, h: 1, color: "yellow", direction: "both" },
+      { id: 10, x: 2, y: 4, w: 1, h: 1, color: "yellow", direction: "both" }
     ]
   }
 ];
